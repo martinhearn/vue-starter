8ff6581a2737f720d9a9fd2024393b840eb17271
Martin Hearn <martinhearn@gmail.com>
2016-04-28 12:37:47 +0100

    source html/js/css

---
+++ src/js/app.vue
@@ -0,0 +1,21 @@
+<style>
+
+  .red {
+    color: #f00;
+  }
+
+</style>
+
+<template>
+  <h1 class="red">{{msg}}</h1>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello World!'
+    }
+  }
+}
+</script>
---
+++ src/js/vue.js
@@ -0,0 +1,13 @@
+var Vue = require('vue')
+var App = require('./app.vue')
+
+// var $ = require('jquery')
+// require('bootstrap')
+
+
+new Vue({
+  el: 'body',
+  components: {
+    app: App
+  }
+})
